21a45a483d2dc04518de35ae11fd5df6be5e7c96
hefour <jhec0113@naver.com>
2026-04-05 15:45:06 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/db_5/db_5_backend/Db5BackendApplication.java
@@ -0,0 +1,13 @@
+package com.db_5.db_5_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Db5BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Db5BackendApplication.class, args);
+	}
+
+}
